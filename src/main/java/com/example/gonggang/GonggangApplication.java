e5e917c601ddb0219cacdee3461ffa77aa35281a
rootTiket <l20078829@gmail.com>
2024-12-01 22:04:12 +0900

    Initial commit

---
+++ src/main/java/com/example/gonggang/GonggangApplication.java
@@ -0,0 +1,13 @@
+package com.example.gonggang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gonggang/GonggangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gonggang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
