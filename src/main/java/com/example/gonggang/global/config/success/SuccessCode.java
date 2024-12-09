400997af838dcefa5a74cabdef528c4f15751fbb
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-12-10 06:22:18 +0900

    feat: 성공 코드 추가
    - `ADMIN_REGISTER_SUCCESS`: 관리자 등록 성공 코드 (HTTP 201)
    - `SIGN_OUT_SUCCESS`: 로그아웃 성공 코드 (HTTP 200)

--- src/main/java/com/example/gonggang/global/config/success/SuccessCode.java
+++ src/main/java/com/example/gonggang/global/config/success/SuccessCode.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
-    ;
+
+    ADMIN_REGISTER_SUCCESS(201, "ADMIN_REGISTER_SUCCESS", "관리자 등록 성공"),
+    SIGN_OUT_SUCCESS(200, "SIGN_OUT_SUCCESS","로그아웃 성공");
+
     private final int httpStatus;
     private final String code;
     private final String message;
