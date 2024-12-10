85b068f02308547e3a5e2ac3e1f8b0acd6fa8960
DongHoon Lee <dhl1924@naver.com>
2024-12-10 18:43:14 +0900

    feat: 공강 레포지토리 추가

---
+++ src/main/java/com/example/gonggang/domain/free_time/repository/FreeTimeRepository.java
@@ -0,0 +1,9 @@
+package com.example.gonggang.domain.free_time.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gonggang.domain.free_time.domain.FreeTime;
+
+public interface FreeTimeRepository extends JpaRepository<FreeTime, Long> {
+
+}
