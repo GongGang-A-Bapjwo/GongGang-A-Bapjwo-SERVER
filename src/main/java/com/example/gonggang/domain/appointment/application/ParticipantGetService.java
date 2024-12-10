31461104ca0af96c71f0628d9e2c8119440a5bf7
rootTiket <l20078829@gmail.com>
2024-12-10 23:23:52 +0900

    feat: 참여하고 있는 방이 있는지 확인하는 로직 구현

--- src/main/java/com/example/gonggang/domain/appointment/application/ParticipantGetService.java
+++ src/main/java/com/example/gonggang/domain/appointment/application/ParticipantGetService.java
@@ -1,6 +1,8 @@
 package com.example.gonggang.domain.appointment.application;
 
 import com.example.gonggang.domain.appointment.domain.AppointmentParticipant;
+import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
+import com.example.gonggang.domain.appointment.exception.ParticipantNotFoundException;
 import com.example.gonggang.domain.appointment.repository.AppointmentParticipantRepository;
 import com.example.gonggang.domain.users.domain.Users;
 import java.util.List;
@@ -12,11 +14,16 @@ import org.springframework.stereotype.Service;
 public class ParticipantGetService {
     private final AppointmentParticipantRepository appointmentParticipantRepository;
 
-    public boolean checkAlreadyEntered(Users users) {
-        return appointmentParticipantRepository.existsByParticipant(users);
+    public boolean checkAlreadyEntered(Users users, AppointmentRoom appointmentRoom) {
+        return appointmentParticipantRepository.existsByParticipantAndAppointmentRoom(users, appointmentRoom);
     }
 
     public List<AppointmentParticipant> findAllByParticipants(Users user) {
-        return appointmentParticipantRepository.findAllByParticipant(user);
+        return appointmentParticipantRepository.findAllByParticipantAndNotLeft(user);
+    }
+
+    public AppointmentParticipant findByParticipantAndRoom(Users user, Long roomId) {
+        return appointmentParticipantRepository.findByParticipantAndRoomId(user,roomId).orElseThrow(
+                ParticipantNotFoundException::new);
     }
 }
