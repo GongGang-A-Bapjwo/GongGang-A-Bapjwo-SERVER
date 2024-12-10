54e3131f83116845c301f1ab4b6838a2ca4c9f9d
rootTiket <l20078829@gmail.com>
2024-12-10 23:21:16 +0900

    feat: 공강팟에 들어가 있는 유저를 가져오는 로직 구현

--- src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentParticipantRepository.java
+++ src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentParticipantRepository.java
@@ -5,14 +5,22 @@ import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
 import com.example.gonggang.domain.users.domain.Users;
 import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AppointmentParticipantRepository extends JpaRepository<AppointmentParticipant, Long> {
-    boolean existsByParticipant(Users participant);
+    boolean existsByParticipantAndAppointmentRoom(Users participant, AppointmentRoom appointmentRoom);
 
     @Query("SELECT ap FROM AppointmentParticipant ap " +
             "JOIN FETCH ap.appointmentRoom ar " +
             "WHERE ap.participant = :participant")
-    List<AppointmentParticipant> findAllByParticipant(@Param("participant") Users participant);
+    List<AppointmentParticipant> findAllByParticipantAndNotLeft(@Param("participant") Users participant);
+
+    @Query("SELECT ap FROM AppointmentParticipant ap " +
+            "WHERE ap.participant = :participant AND ap.appointmentRoom.id = :roomId")
+    Optional<AppointmentParticipant> findByParticipantAndRoomId(
+            @Param("participant") Users participant,
+            @Param("roomId") Long roomId
+    );
 }
