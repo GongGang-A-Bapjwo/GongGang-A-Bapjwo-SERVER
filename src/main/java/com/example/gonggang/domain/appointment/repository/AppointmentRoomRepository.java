e45e07de7b15cc8c42b5266042007de121e5d0c4
rootTiket <l20078829@gmail.com>
2024-12-10 18:02:00 +0900

    feat: 참여코드를 통해 공강팟을 찾는 로직 구현

--- src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentRoomRepository.java
+++ src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentRoomRepository.java
@@ -1,7 +1,9 @@
 package com.example.gonggang.domain.appointment.repository;
 
 import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRoomRepository extends JpaRepository<AppointmentRoom, Long> {
+    Optional<AppointmentRoom> findByEntranceCode(String enteranceCode);
 }
