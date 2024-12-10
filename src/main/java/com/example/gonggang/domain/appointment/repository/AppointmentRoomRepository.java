e8c84f08a13e4aa8efc1981660019717a9265eed
rootTiket <l20078829@gmail.com>
2024-12-10 16:08:11 +0900

    feat: 필요한 리포지토리 구현

---
+++ src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentRoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.gonggang.domain.appointment.repository;
+
+import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRoomRepository extends JpaRepository<AppointmentRoom, Long> {
+}
