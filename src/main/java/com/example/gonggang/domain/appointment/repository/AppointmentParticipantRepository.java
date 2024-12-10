b0332759352ed789fe6bd3e1ee08b513b115b2de
rootTiket <l20078829@gmail.com>
2024-12-10 22:07:39 +0900

    feat: 참여자를 모두 가져오는 로직을 구현

--- src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentParticipantRepository.java
+++ src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentParticipantRepository.java
@@ -1,9 +1,18 @@
 package com.example.gonggang.domain.appointment.repository;
 
 import com.example.gonggang.domain.appointment.domain.AppointmentParticipant;
+import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
 import com.example.gonggang.domain.users.domain.Users;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AppointmentParticipantRepository extends JpaRepository<AppointmentParticipant, Long> {
     boolean existsByParticipant(Users participant);
+
+    @Query("SELECT ap FROM AppointmentParticipant ap " +
+            "JOIN FETCH ap.appointmentRoom ar " +
+            "WHERE ap.participant = :participant")
+    List<AppointmentParticipant> findAllByParticipant(@Param("participant") Users participant);
 }
