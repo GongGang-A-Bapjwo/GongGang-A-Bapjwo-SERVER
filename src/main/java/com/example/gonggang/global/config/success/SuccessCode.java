502d067a16ca0bd89b5f033757b3a5a1fa9123c2
rootTiket <l20078829@gmail.com>
2024-12-12 17:11:19 +0900

    feat: 시간표 수동 등록 로직 구현

--- src/main/java/com/example/gonggang/global/config/success/SuccessCode.java
+++ src/main/java/com/example/gonggang/global/config/success/SuccessCode.java
@@ -10,7 +10,8 @@ public enum SuccessCode {
     ADMIN_REGISTER_SUCCESS(201, "ADMIN_REGISTER_SUCCESS", "관리자 등록 성공"),
     SIGN_OUT_SUCCESS(200, "SIGN_OUT_SUCCESS","로그아웃 성공"),
     ENTER_SUCCESS(200,"ENTER_SUCCESSED","공강팟 입장 성공"),
-    UPDATE_SUCCESS(200, "UPDATE_SUCCESS", "업데이트 성공");
+    UPDATE_SUCCESS(200, "UPDATE_SUCCESS", "업데이트 성공"),
+    CREATE_SUCCESS(200,"CREATE_SUCCESS","생성 성공");
 
     private final int httpStatus;
     private final String code;
