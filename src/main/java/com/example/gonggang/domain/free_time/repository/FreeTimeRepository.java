14866c0cabf2bc12985f222cfb82be0342551bbd
rootTiket <l20078829@gmail.com>
2024-12-12 16:54:09 +0900

    feat: 공강 조회로직 구현

--- src/main/java/com/example/gonggang/domain/free_time/repository/FreeTimeRepository.java
+++ src/main/java/com/example/gonggang/domain/free_time/repository/FreeTimeRepository.java
@@ -1,9 +1,16 @@
 package com.example.gonggang.domain.free_time.repository;
 
+import com.example.gonggang.domain.common.Weekday;
+import com.example.gonggang.domain.users.domain.Users;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.gonggang.domain.free_time.domain.FreeTime;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FreeTimeRepository extends JpaRepository<FreeTime, Long> {
 
+    @Query("SELECT f FROM FreeTime f WHERE f.user = :user AND f.weekday = :weekday")
+    List<FreeTime> findAllByUserAndWeekday(@Param("user") Users user, @Param("weekday") Weekday weekday);
 }
