e8a9c13602e9d16f31a8a646b0eba19b632898fd
rootTiket <l20078829@gmail.com>
2024-12-12 18:08:54 +0900

    fix: 타입 통일

--- src/main/java/com/example/gonggang/domain/free_time/dto/request/FreeTimeRequestItem.java
+++ src/main/java/com/example/gonggang/domain/free_time/dto/request/FreeTimeRequestItem.java
@@ -1,7 +1,9 @@
 package com.example.gonggang.domain.free_time.dto.request;
 
+import com.example.gonggang.domain.common.Weekday;
+
 public record FreeTimeRequestItem(
-        String weekday,
+        Weekday weekday,
         String startTime,
         String endTime
 ) {
