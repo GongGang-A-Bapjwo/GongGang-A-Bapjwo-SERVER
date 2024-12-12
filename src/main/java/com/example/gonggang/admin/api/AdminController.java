491ea99216f15bbc1ccf6178fef0d795c8409abc
rootTiket <l20078829@gmail.com>
2024-12-12 13:22:45 +0900

    feat: 관리자 전체 유저 조회 구현

--- src/main/java/com/example/gonggang/admin/api/AdminController.java
+++ src/main/java/com/example/gonggang/admin/api/AdminController.java
@@ -1,7 +1,13 @@
 package com.example.gonggang.admin.api;
 
+import com.example.gonggang.admin.application.UserManageService;
+import com.example.gonggang.admin.application.dto.response.ReadAllMemberResponse;
+import com.example.gonggang.global.auth.annotation.CurrentMember;
+import feign.Response;
+import java.util.List;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,7 @@ public class AdminController implements AdminApi {
 	private static final int COOKIE_MAX_AGE = 7 * 24 * 60 * 60;
 	private static final String REFRESH_TOKEN = "refreshToken";
 	private final AdminAuthService adminAuthService;
+	private final UserManageService userManageService;
 
 	@Override
 	public ResponseEntity<String> register(AdminRegisterRequest request) {
@@ -46,5 +53,11 @@ public class AdminController implements AdminApi {
 	@Override
 	public ResponseEntity<String> healthCheck(){
 		return ResponseEntity.ok().body("ok");
+	}
+
+	@GetMapping("/get-members")
+	public ResponseEntity<List<ReadAllMemberResponse>> findAll() {
+		List<ReadAllMemberResponse> response = userManageService.readAll();
+		return ResponseEntity.ok(response);
 	}
 }
