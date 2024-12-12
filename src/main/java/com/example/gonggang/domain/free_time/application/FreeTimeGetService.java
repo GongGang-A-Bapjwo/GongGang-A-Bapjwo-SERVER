95c3ce441a641d0113ef002472b50d024b69014e
rootTiket <l20078829@gmail.com>
2024-12-12 16:54:09 +0900

    feat: 공강 조회로직 구현

---
+++ src/main/java/com/example/gonggang/domain/free_time/application/FreeTimeGetService.java
@@ -0,0 +1,19 @@
+package com.example.gonggang.domain.free_time.application;
+
+import com.example.gonggang.domain.common.Weekday;
+import com.example.gonggang.domain.free_time.domain.FreeTime;
+import com.example.gonggang.domain.free_time.repository.FreeTimeRepository;
+import com.example.gonggang.domain.users.domain.Users;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FreeTimeGetService {
+    private final FreeTimeRepository freeTimeRepository;
+
+    public List<FreeTime> findAllByUserAndWeekDay(Users user, Weekday weekday) {
+        return freeTimeRepository.findAllByUserAndWeekday(user, weekday);
+    }
+}
