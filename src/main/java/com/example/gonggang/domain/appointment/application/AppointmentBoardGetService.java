0ee6b21b9f818e434d60cd90fe97daa0cd6877f9
rootTiket <l20078829@gmail.com>
2024-12-12 16:53:52 +0900

    feat: 시간과 날짜로 게시판 조회하는 로직 구현

---
+++ src/main/java/com/example/gonggang/domain/appointment/application/AppointmentBoardGetService.java
@@ -0,0 +1,18 @@
+package com.example.gonggang.domain.appointment.application;
+
+import com.example.gonggang.domain.appointment.domain.AppointmentBoard;
+import com.example.gonggang.domain.appointment.repository.AppointmentBoardRepository;
+import com.example.gonggang.domain.free_time.domain.FreeTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentBoardGetService {
+    private final AppointmentBoardRepository appointmentBoardRepository;
+
+    public List<AppointmentBoard> findAllBoard(FreeTime freeTime) {
+        return appointmentBoardRepository.findAllByStartTimeAndEndTimeAndWeekday(freeTime.getStartTime(),freeTime.getEndTime(), freeTime.getWeekday());
+    }
+}
---
+++ src/main/java/com/example/gonggang/domain/appointment/repository/AppointmentBoardRepository.java
@@ -0,0 +1,12 @@
+package com.example.gonggang.domain.appointment.repository;
+
+import com.example.gonggang.domain.appointment.domain.AppointmentBoard;
+import com.example.gonggang.domain.common.Weekday;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentBoardRepository extends JpaRepository<AppointmentBoard,Long> {
+    List<AppointmentBoard> findAllByStartTimeAndEndTimeAndWeekday(LocalTime startTime, LocalTime endTime,
+                                                                  Weekday weekday);
+}
