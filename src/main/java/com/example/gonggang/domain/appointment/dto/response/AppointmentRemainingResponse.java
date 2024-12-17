57ee1d79348d51d3f1f5e42d395bbb6982ce3bd7
rootTiket <l20078829@gmail.com>
2024-12-17 17:26:44 +0900

    HOTFIX: dto 수정

--- src/main/java/com/example/gonggang/domain/appointment/dto/response/AppointmentRemainingResponse.java
+++ src/main/java/com/example/gonggang/domain/appointment/dto/response/AppointmentRemainingResponse.java
@@ -5,13 +5,15 @@ import com.example.gonggang.domain.appointment.domain.AppointmentRoom;
 public record AppointmentRemainingResponse(
         long roomId,
         int currentUserCount,
-        int remainingCount
+        int remainingCount,
+        String entranceCode
 ) {
     public static AppointmentRemainingResponse toResponse(AppointmentRoom room) {
         return new AppointmentRemainingResponse(
                 room.getId(),
                 room.getMaxParticipants(),
-                room.getMaxParticipants() - room.getCurrentParticipants()
+                room.getMaxParticipants() - room.getCurrentParticipants(),
+                room.getEntranceCode()
         );
     }
 }
